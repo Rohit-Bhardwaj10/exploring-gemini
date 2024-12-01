6ad0b12e0a9c3460a0012358b72d3e38abdbe68b
rohit bhardwaj <rb8329563@gmail.com>
2024-12-01 23:34:42 +0530

    almost done

--- gemini chatbot/src/App.jsx
+++ gemini chatbot/src/App.jsx
@@ -1,15 +1,46 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+
+  const[search,setSearch]=useState("")
+  const[answer,setAnswer]=useState("")
+
+  const API_KEY="AIzaSyBeeJrm-5bP5iEc0OAKnWXAQcrEAc78Oyc"
+  const API_URL=`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${API_KEY}`
+  const generateResponse= async ()=>{
+    setAnswer("loading...")
+    const response=await axios({
+      url:API_URL,
+      method:"POST",
+      data:{
+        contents:[
+          {
+            parts:[{
+              text:`${search}`,
+            }]
+          }
+        ]
+
+      }
+    })
+
+    setAnswer(response['data']['candidates'][0]['content']['parts'][0]['text']);
+    setSearch("")
+  }
+
 
   return (
-    <>
-     
-    </>
+    <div className='main'>
+     <h1>AI CHAT</h1>
+     <textarea value={search} onChange={(e)=>setSearch(e.target.value)}></textarea>
+     <button onClick={generateResponse}>generate answer</button>
+     <pre>
+      {answer}
+     </pre>
+    </div>
   )
 }
 
